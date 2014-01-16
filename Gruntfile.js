a5846724fbe91f382e90fa211cfa1e5be139a797
Matty <matty@mattycollins.com.au>
2014-01-16 13:20:56 +1100

    it builds \!

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'standard/scripts/main.min.js': [
+            'standard/scripts/init.js',
+            'standard/scripts/helpers.js',
+            'standard/scripts/cr03.js',
+            'standard/scripts/breadcrumb.js'
+          ]
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+
+};
