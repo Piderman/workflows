e710574be3f9127a3423e990849799c85590aa61
Matty <matty@mattycollins.com.au>
2014-01-17 11:01:21 +1100

    development and production modes

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,11 +17,29 @@ module.exports = function(grunt) {
           ]
         }
       }
-    }
+    },
+
+    //
+    sass: {
+      dev: {
+        options: {
+          style: "expanded",
+          sourcemap: true
+        },
+        files : { "standard/styles/root.css" : "standard/styles/root.scss"}
+      },
+      prod: {
+        options: {
+          style: "compressed"
+        },
+        files : { "standard/styles/root.css" : "standard/styles/root.scss"}
+      }
+    } 
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
   // grunt.registerTask('default', ['uglify']);
